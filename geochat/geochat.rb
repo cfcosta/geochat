c652acb330caf87200bc63e3a3aa53b007000709
Cainã Costa <cainan.costa@gmail.com>
2011-12-04 01:40:12 -0200

    Adicionando logging no GeoChat.

--- geochat/geochat.rb
+++ geochat/geochat.rb
@@ -3,26 +3,31 @@ require 'em-websocket'
 
 module GeoChat
   def start_server
+    logger.info("Starting reactor...")
     EventMachine.run do
+      logger.info("Reactor started!")
+      logger.info("Started GeoChat on host %s and port %d" % [GeoChat::Config.host, GeoChat::Config.port])
+
       EventMachine::WebSocket.start(host: GeoChat::Config.host, port: GeoChat::Config.port) do |ws|
         ws.onopen do
-          puts "WebSocket connection open"
-
-          # publish message to the client
-          ws.send "Hello Client"
+          logger.debug("New connection initiated.")
         end
 
         ws.onclose do
-          puts "Connection closed"
+          logger.debug("Connection closed...")
         end
 
         ws.onmessage do |msg|
-          puts "Recieved message: #{msg}"
+          logger.debug("Received message %s" % msg)
           ws.send "Pong: #{msg}"
         end
       end
     end
   end
 
-  module_function :start_server
+  def logger
+    GeoChat::Config.logger
+  end
+
+  module_function :start_server, :logger
 end
